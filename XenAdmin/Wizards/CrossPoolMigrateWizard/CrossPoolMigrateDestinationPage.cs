ca8b4735a5b597aa076ec99a6d01af612af562c4
lukun <kun.lu@halsign.com>
2016-10-30 13:40:29 +0800

    1. remove the halsign home link in about 2. disable encrypt crosspool migrate 3. NIC GPU localizaion

--- XenAdmin/Wizards/CrossPoolMigrateWizard/CrossPoolMigrateDestinationPage.cs
+++ XenAdmin/Wizards/CrossPoolMigrateWizard/CrossPoolMigrateDestinationPage.cs
@@ -73,7 +73,7 @@ namespace XenAdmin.Wizards.CrossPoolMigrateWizard
 
         protected override bool IsShowEncryption
         {
-            get { return true; }
+            get { return false; }
         }
 
         protected override bool ImplementsIsDirty()
