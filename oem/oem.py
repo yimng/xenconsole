8b1faac59919bff1ba9c4897a24d8db3482dc89d
lukun <kun.lu@halsign.com>
2016-11-25 10:24:13 +0800

    add oem files

---
+++ oem/oem.py
@@ -0,0 +1,55 @@
+#!/bin/python
+
+from os import path
+from shutil import copyfile
+from io import open
+
+resxfiles = "oem_files.conf"
+corpconf = "oem.properties"
+rcconf = "oem_resources.conf"
+src = path.abspath("resources")
+dst = path.abspath("..")
+
+# get oem name
+old = []
+new = []
+cf = open(corpconf, encoding='utf-8')
+for line in cf:
+	if line.startwith('#'):
+		continue
+    (k, v) = line.split('=')
+    old.append(k)
+    new.append(v.rstrip())
+    if k == 'halsign':
+        oemname = v.rstrip()
+cf.close()
+
+# replace the resx files
+ff = open(resxfiles)
+encode = 'ascii'
+for line in ff:
+	print(path.join(dst, path.normpath(line.rstrip())))
+	try:
+		f = open(path.join(dst, path.normpath(line.rstrip())), 'r')
+		s = f.read()
+		f.close()		
+		f = open(path.join(dst, path.normpath(line.rstrip())), 'r+')
+		for o,n in zip(old, new):
+			s = s.replace(o, n)
+		f.write(s)
+	except:
+		f = open(path.join(dst, path.normpath(line.rstrip())), 'r', encoding='utf-8')
+		s = f.read()
+		f.close()
+		f = open(path.join(dst, path.normpath(line.rstrip())), 'r+', encoding='utf-8')
+		for o,n in zip(old, new):
+			s = s.replace(o, n)
+		f.write(s)
+	f.close()
+ff.close()
+
+# replace the images
+rf = open(rcconf)
+for line in rf:
+    (k, v) = line.split('=')
+    copyfile(path.join(src, path.normpath(k)), path.join(dst, path.normpath(v.rstrip().replace("halsign", oemname))))
